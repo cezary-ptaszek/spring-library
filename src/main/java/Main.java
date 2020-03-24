156007de792cae4c60d3ba1d3c69ce61ee12b4d0
cezary-ptaszek <cezaryptaszek@gmail.com>
2020-03-24 22:58:16 +0100

    init vertx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 import Helpers.ParsingJsonHelper;
 import Models.FirstParents.Item;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 import java.io.FileNotFoundException;
 import java.util.List;
@@ -7,5 +11,23 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
      List<Item> items = ParsingJsonHelper.parse("src\\main\\resources\\books.json");
+
+        Vertx vertx = Vertx.vertx();
+        HttpServer httpServer = vertx.createHttpServer();
+
+        Router router = Router.router(vertx);
+
+        router
+                .get("/book/details")
+                .handler(routingContext -> {
+                    HttpServerResponse response = routingContext.response();
+                    response.putHeader("content-type", "text/plain");
+                    response.end("Hello");
+        });
+
+        httpServer
+                .requestHandler(router)
+                .listen(8080);
+        System.out.println("Server started at: " + httpServer.actualPort());
     }
 }
