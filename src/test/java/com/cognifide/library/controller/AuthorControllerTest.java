835ab0a1b287d306ace194d55a3d4f94a61cf8a7
cezary-ptaszek <cezaryptaszek@gmail.com>
2020-03-27 22:53:34 +0100

    up tests

---
+++ src/test/java/com/cognifide/library/controller/AuthorControllerTest.java
@@ -0,0 +1,34 @@
+package com.cognifide.library.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+class AuthorControllerTest {
+
+    @Test
+    void getAuthorsRating(){
+        get("/author/rating").then().assertThat().statusCode(200);
+    }
+
+    @Test
+    void getAuthorByName() {
+        when()
+                .get("/author/rating?name=Jain Pravin").
+        then()
+                .statusCode(200)
+                .body("name", equalTo("Jain Pravin"),
+                        "rating", equalTo(5.0f),
+                        "allCounter", equalTo(9),
+                        "currCounter",equalTo(0));
+
+    }
+
+    @Test
+    void notGetAuthorByName() {
+        get("/author/rating?name=Adam Mickiewicz").then().assertThat().statusCode(404);
+
+    }
+
+}
---
+++ src/test/java/com/cognifide/library/controller/ItemControllerTest.java
@@ -0,0 +1,29 @@
+package com.cognifide.library.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemControllerTest {
+
+    @Test
+    void hello() {
+        get("/item/").then().assertThat().statusCode(200);
+    }
+
+    @Test
+    void getItemByIsbn() {
+        get("/item?isbn=9780226285108").then().assertThat().statusCode(200);
+    }
+
+    @Test
+    void notGetItemByIsbn() {
+        get("/item?isbn=9788392789109").then().assertThat().statusCode(404);
+    }
+
+    @Test
+    void getItemsByCategory() {
+        get("/item?category=Religion").then().assertThat().statusCode(200);
+    }
+}
